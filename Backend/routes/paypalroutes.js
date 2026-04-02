20a5e3a16f7ee5337815314157e3f2de5da87671
Ananya Jan <ananyajan2000@gmail.com>
2026-04-02 10:42:39 +0530

    removed authmiddleware from paypal routes

--- Backend/routes/paypalroutes.js
+++ Backend/routes/paypalroutes.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import {createPayPalOrder, capturePayPalOrder } from '../controllers/paypalController.js';
-import authMiddleware from '../middleware/authMiddleware.js';
+// import authMiddleware from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/paypal/create-order', createPayPalOrder);
-router.post('/paypal/capture/:orderId',authMiddleware, capturePayPalOrder);
+router.post('/paypal/capture/:orderId', capturePayPalOrder);
 
 export default router
